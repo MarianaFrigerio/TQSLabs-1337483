3baf6271014b323a19b06710bed98268cbea7315
Mariana Frigerio <maryfrigerio@gmail.com>
2018-10-14 21:03:18 +0200

    Tests iniciales y estructura interna

--- InternalGame.java
+++ InternalGame.java
@@ -2,4 +2,81 @@ package mastermind;
 
 public class InternalGame {
 
+	/*Variables of the game*/
+	
+	//Constants of checkCode
+	static final int TOTAL_HIT_BLACK = 2;
+	static final int HIT_WHITE = 1;
+	static final int FAIL = 0;
+	
+	//Constants of the colors of the game
+	static final int BLUE = 1;
+	static final int YELLOW = 2;
+	static final int RED = 3;
+	static final int GREEN = 4;
+	static final int PURPLE = 5;
+	static final int ORANGE = 6;
+	
+	//Private variables
+	private String[] initialUserCode = new String [4];
+	private String[] finalSecretCode = new String [4];
+	private String[] formalizedCheckCode = new String[4];
+	private int[] userCode = new int [4];
+	private int[] secretCode = new int[4];
+	private int[] checkCode = new int [4];
+	private int tries;
+	
+	public int[] getUserCode() {
+		return userCode;
+	}
+	
+	public int[] setUserCode(String[] initialStringUserCode) { 
+		return this.userCode;
+	}
+	
+	public int[] getSecretCode() { //solo numeros
+		return secretCode;
+	}
+	
+	public void setSecretCode(int[] secretCode) { //llenar el vector/array con numeros random
+		this.secretCode = secretCode;
+	}
+	
+	public String[] convertSecretCode(int[] secretInt) { //de numeros random a palabras random
+		return this.finalSecretCode;
+	}
+	
+	public int[] getCheckCode() {
+		return checkCode;
+	}
+	
+	public void setCheckCode(int[] userCode, int[] secretCode) {
+		int[] codeCheck = new int[4];
+		this.checkCode = codeCheck;
+	}
+	
+	public int getTries() {
+		return tries;
+	}
+	
+	public void setTries(int tries) {
+		this.tries = tries;
+	}
+	
+	public String[] getInitialUserCode() {
+		return initialUserCode;
+	}
+	
+	public String[] setInitialUserCode(String voidUserCode) {
+		return initialUserCode;
+	}
+
+	public String[] getFormalizedCheckCode() {
+		return formalizedCheckCode;
+	}
+
+	public void setFormalizedCheckCode(int[] initialCheckCode) {
+		String[] formCheck = new String[4];
+		this.formalizedCheckCode = formCheck;
+	}
 }
--- InternalGameTest.java
+++ InternalGameTest.java
@@ -12,8 +12,76 @@ class InternalGameTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetAndSetUserCode() {
+		InternalGame intern1 = new InternalGame ();
+		String[] initialStringUserCode = {"red", "blue"};
+		intern1.setUserCode(initialStringUserCode);
+		
+		
+		
+		assertEquals(intern1.getUserCode().length, 4);
+		assertEquals(intern1.getUserCode()[0], 3);
+		assertEquals(intern1.getUserCode()[1], 1);
+		assertEquals(intern1.getUserCode()[2], 0);
+		assertEquals(intern1.getUserCode()[3], 0);
 	}
 
+	@Test
+	void testGetAndSetSecretCode() {
+		InternalGame intern2 = new InternalGame ();
+		int[] secr1 = {0, 0, 0, 0};
+		intern2.setSecretCode(secr1);
+		assertEquals(intern2.getSecretCode().length, 4);
+	}
+
+	@Test
+	void testConvertSecretCode() {
+		InternalGame intern3 = new InternalGame ();
+		int[] secr1 = {1, 2, 3, 4};
+		assertTrue(intern3.convertSecretCode(secr1)[0] == "blue" && intern3.convertSecretCode(secr1)[1] == "yellow" &&
+				   intern3.convertSecretCode(secr1)[2] == "red" && intern3.convertSecretCode(secr1)[3] == "green");
+	}
+
+	@Test
+	void testGetAndSetCheckCode() {
+		InternalGame intern4 = new InternalGame ();
+		int[] secr1 = {1,2,3,4};
+		int[] usr1 = {1,2,3,4};
+		intern4.setCheckCode(usr1, secr1);
+		int[] check1 = {2,2,2,2};
+		assertArrayEquals(intern4.getCheckCode(), check1);
+	}
+
+	@Test
+	void testGetAndSetTries() {
+		InternalGame intern5 = new InternalGame ();
+		
+		intern5.setTries(1);
+		assertTrue(intern5.getTries() == 1);	
+		
+		intern5.setTries(-1);
+		assertTrue(intern5.getTries() == 0);		
+		
+	}
+
+	@Test
+	void testGetAndSetInitialUserCode() {
+		InternalGame intern6 = new InternalGame ();
+		String initUsCode = "blue red";
+		String[] initStrUsCode = {"blue", "red"};
+		intern6.setInitialUserCode(initUsCode);
+		assertArrayEquals(intern6.getInitialUserCode(), initStrUsCode);
+	}
+
+	@Test
+	void testGetAndSetFormalizedCheckCode() {
+		InternalGame intern7 = new InternalGame ();
+		int[] secr1 = {1,2,3,4};
+		int[] usr1 = {1,2,3,4};
+		intern7.setCheckCode(usr1, secr1);		
+		
+		intern7.setFormalizedCheckCode(intern7.getCheckCode());
+		String[] formCheck1 = {"black", "black", "black", "black"};
+		assertArrayEquals(intern7.getFormalizedCheckCode(), formCheck1);
+	}
 }
